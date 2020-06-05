7b7105bd74b284f14ad3f3a26d2d32da445a362d
willensoll <william.ensoll@gmail.com>
2020-06-05 11:36:22 +0100

    first commit

---
+++ js/magic-8-ball.js
@@ -0,0 +1,16 @@
+var myShakeEvent = new Shake({
+    threshold: 15, // optional shake strength threshold
+    timeout: 1000 // optional, determines the frequency of event generation
+});
+
+myShakeEvent.start();
+
+window.addEventListener('shake', shakeEventDidOccur, false);
+
+//function to call when shake occurs
+function shakeEventDidOccur () {
+
+    //put your own code here etc.
+    alert('shake!');
+}
+
