b937a7ada5ce4ce27323da1d0d036d94a06bf663
willensoll <william.ensoll@gmail.com>
2020-06-05 13:08:51 +0100

    add quotes

--- js/magic-8-ball.js
+++ js/magic-8-ball.js
@@ -1,3 +1,9 @@
+
+const quoteArray = ["Love For All, Hatred For None", "Change the world by being yourself", "Every moment is a fresh beginning",
+ "Never regret anything that made you smile"
+, "Aspire to inspire before we expire", "Everything you can imagine is real"];
+
+
 var myShakeEvent = new Shake({
     threshold: 15, // optional shake strength threshold
     timeout: 1000 // optional, determines the frequency of event generation
@@ -9,8 +15,10 @@ window.addEventListener('shake', shakeEventDidOccur, false);
 
 //function to call when shake occurs
 function shakeEventDidOccur () {
-
     //put your own code here etc.
-    alert('shake!');
+    alert(quoteArray[getRandomInt(5)]);
 }
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
